f3c52abb9ed61f12bdbac22a715276c0d8d5502b
Nick Metz <nick.metz@ptvgroup.com>
2021-08-13 09:23:16 +0200

    add addistional https checks

--- uptrends/monitor_web_resource_test.go
+++ uptrends/monitor_web_resource_test.go
@@ -22,28 +22,34 @@ func TestAccUptrendsMonitorWeb_Basic(t *testing.T) {
 			{
 				Config: r.basic(randInt),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckUptrendsMonitorWebDestroyExists("uptrends_monitor_web.test"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "name", fmt.Sprintf("acctest-uptrends-monitor-web-%d", randInt)),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "url", "http://example.com/"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "mode", "Production"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "user_agent", UserAgent("chrome83")),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "auth_type", "None"),
+					testAccCheckUptrendsMonitorWebDestroyExists("uptrends_monitor_web.http"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "name", fmt.Sprintf("acctest-uptrends-monitor-web-http-%d", randInt)),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "url", "http://example.com/"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "mode", "Production"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "user_agent", UserAgent("chrome83")),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "auth_type", "None"),
+					testAccCheckUptrendsMonitorWebDestroyExists("uptrends_monitor_web.https"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.https", "name", fmt.Sprintf("acctest-uptrends-monitor-web-https-%d", randInt)),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.https", "url", "https://example.com/"),
 				),
 			},
 			{
 				Config: r.update(randInt),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckUptrendsMonitorWebDestroyExists("uptrends_monitor_web.test"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "name", fmt.Sprintf("acctest-uptrends-monitor-web-%d", randInt)),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "url", "http://example.org/"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "mode", "Staging"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "expected_http_status_code", "200"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "user_agent", UserAgent("chrome83_android")),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "auth_type", "Basic"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "username", "user"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "password", "secret"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "alert_on_min_bytes", "true"),
-					resource.TestCheckResourceAttr("uptrends_monitor_web.test", "min_bytes", "1024"),
+					testAccCheckUptrendsMonitorWebDestroyExists("uptrends_monitor_web.http"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "name", fmt.Sprintf("acctest-uptrends-monitor-web-http-%d", randInt)),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "url", "http://example.org/"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "mode", "Staging"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "expected_http_status_code", "200"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "user_agent", UserAgent("chrome83_android")),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "auth_type", "Basic"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "username", "user"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "password", "secret"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "alert_on_min_bytes", "true"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.http", "min_bytes", "1024"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.https", "name", fmt.Sprintf("acctest-uptrends-monitor-web-https-%d", randInt)),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.https", "url", "https://example.org/"),
+					resource.TestCheckResourceAttr("uptrends_monitor_web.https", "check_cert_errors", "false"),
 				),
 			},
 		},
@@ -55,7 +61,7 @@ func testAccUptrendsMonitorWebDestroyCheck(s *terraform.State) error {
 	auth := testAccProvider.Meta().(*Uptrends).AuthContext
 
 	for _, r := range s.RootModule().Resources {
-		if r.Type != "uptrends_monitor_wev" {
+		if r.Type != "uptrends_monitor_web" {
 			continue
 		}
 
@@ -99,17 +105,22 @@ func testAccCheckUptrendsMonitorWebDestroyExists(n string) resource.TestCheckFun
 
 func (r UptrendsMonitorWebResource) basic(randInt int) string {
 	return fmt.Sprintf(`
-resource "uptrends_monitor_web" "test" {
-  name = "acctest-uptrends-monitor-web-%d"
+resource "uptrends_monitor_web" "http" {
+  name = "acctest-uptrends-monitor-web-http-%d"
   url  = "http://example.com/"
 }
-`, randInt)
+
+resource "uptrends_monitor_web" "https" {
+  name = "acctest-uptrends-monitor-web-https-%d"
+  url  = "https://example.com/"
+}
+`, randInt, randInt)
 }
 
 func (r UptrendsMonitorWebResource) update(randInt int) string {
 	return fmt.Sprintf(`
-resource "uptrends_monitor_web" "test" {
-  name                       = "acctest-uptrends-monitor-web-%d"
+resource "uptrends_monitor_web" "http" {
+  name                       = "acctest-uptrends-monitor-web-http-%d"
   type                       = "Http"
   url                        = "http://example.org/"
   mode                       = "Staging"
@@ -156,5 +167,55 @@ resource "uptrends_monitor_web" "test" {
     exclude_locations = ["Vancouver"]
   }
 }
-`, randInt)
+
+resource "uptrends_monitor_web" "https" {
+  name                       = "acctest-uptrends-monitor-web-https-%d"
+  type                       = "Https"
+  url                        = "https://example.org/"
+  mode                       = "Staging"
+  generate_alert             = false
+  check_interval             = 10
+  notes                      = "Managed by Terraform"
+  primary_checkpoints_only   = false
+  alert_on_load_time_limit_1 = true
+  load_time_limit_1          = 3000
+  alert_on_load_time_limit_2 = true
+  load_time_limit_2          = 7000
+  alert_on_min_bytes         = true
+  min_bytes                  = 1024
+
+  check_cert_errors         = false
+  expected_http_status_code = 200
+  user_agent                = "chrome83_android"
+  auth_type                 = "Basic"
+  username                  = "user"
+  password                  = "secret"
+
+  request_headers {
+    name  = "X-Test1-Header"
+    value = "true"
+  }
+
+  request_headers {
+    name  = "X-Test2-Header"
+    value = "true"
+  }
+
+  match_pattern {
+    pattern     = "example"
+    is_positive = true
+  }
+
+  match_pattern {
+    pattern     = "foo"
+    is_positive = false
+  }
+
+  selected_checkpoints {
+    regions           = ["Asia", "1005"]
+    checkpoints       = ["Salzburg", "1"]
+    exclude_locations = ["Vancouver"]
+  }
+}
+`, randInt, randInt)
 }
